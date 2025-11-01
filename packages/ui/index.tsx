fba7f712a8c328c4f5c30953f62217e513dfd400
John Carlo Dimaranan <john.dimaranan@encompaas.cloud>
2025-11-02 01:10:53 +0800

    remove unnecessary codes and file, update packages

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,1 +1,10 @@
-export const Hello = () => <p>Hello from Shared UI 👋</p>;
+
+export const Hello = () => (
+  // <Stack ai="center" jc="center" p="$4">
+  //   <Text fontSize="$6" color="$color12">
+  //     Hello from Tamagui + Fit Genie
+  //   </Text>
+  //   <Button>Press me</Button>
+  // </Stack>
+  <p>Hello from Tamagui + Fit Genie</p>
+);
